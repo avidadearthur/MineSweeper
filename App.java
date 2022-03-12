f73dddaed290bab35d1f06e3389077206ed009e1
arthur <arthurtquintao@gmail.com>
2022-03-12 14:53:20 +0100

    Enable tile display when app runs

--- App.java
+++ App.java
@@ -1,20 +1,18 @@
 import model.Difficulty;
 //import model.MineSweeper;
+import model.MineSweeper;
 import model.PlayableMinesweeper;
 import view.MinesweeperView;
 
 public class App {
     public static void main(String[] args) throws Exception {
         MinesweeperView view = new MinesweeperView();
-        //Uncomment the lines below once your game model code is ready; don't forget to import your game model 
-        //PlayableMinesweeper model = new MineSweeper();
-        
-        
+        //Uncomment the lines below once your game model code is ready; don't forget to import your game model
+        PlayableMinesweeper model = new MineSweeper();
         /**
-            Your code to bind your game model to the game user interface
-        */
-        
-        
-        //model.startNewGame(Difficulty.EASY);
+         Your code to bind your game model to the game user interface
+         */
+        model.startNewGame(Difficulty.MEDIUM);
+        view.notifyNewGame(model.getWidth(),model.getHeight());
     }
 }
