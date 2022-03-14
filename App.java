aa454ceb169287e528f4fe07727d06d786a02a3c
arthur <arthurtquintao@gmail.com>
2022-03-14 15:15:38 +0100

    Update App

--- App.java
+++ App.java
@@ -7,9 +7,8 @@ import view.MinesweeperView;
 public class App {
     public static void main(String[] args) throws Exception {
         MinesweeperView view = new MinesweeperView();
-        //Uncomment the lines below once your game model code is ready; don't forget to import your game model
         PlayableMinesweeper model = new MineSweeper();
-        model.startNewGame(Difficulty.MEDIUM);
-        view.notifyNewGame(model.getWidth(),model.getHeight());
+        view.setGameModel(model);
+        model.startNewGame(Difficulty.EASY);
     }
 }
